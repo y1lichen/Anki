5e34a605540cb76b49c6a6960db2c7e9f19c251e
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-02-21 00:54:42 +0800

    Add animation for wrong answer for OptionButton.

--- Anki/View/PlayingView/QuizView.swift
+++ Anki/View/PlayingView/QuizView.swift
@@ -8,27 +8,26 @@
 import SwiftUI
 
 struct QuizView: View {
-    
-	let goBackAndAddItem: () -> Void
+    let goBackAndAddItem: () -> Void
     let itemCount: Int
     private var items: [Item]
-	
+
     @State private var shuffeldItems = [Item]()
 
     @State private var itemIndex: Int = 0 {
         didSet {
             selectedItem = shuffeldItems[itemIndex]
             frequency = Int(selectedItem.frequency)
-			options = getOptions()
+            options = getOptions()
         }
     }
 
     @State private var selectedItem: Item
     @State private var frequency: Int = 0
 
-	@State private var isWrong: Bool = false
-	
-	@State private var options = [String]()
+    @State private var isWrong: Bool = false
+
+    @State private var options = [String]()
 
     init(items: [Item], goBackAndAddItem: @escaping () -> Void) {
         itemCount = items.count
@@ -45,26 +44,26 @@ struct QuizView: View {
             AltView(goBackAndAddItem: goBackAndAddItem)
         } else {
             VStack {
-				Spacer()
+                Spacer()
                 Text(selectedItem.front!)
                     .font(.system(size: 50))
                     .fontWeight(.bold)
                     .padding(.top, 10)
                 Spacer()
-					.frame(height: 60)
-				ForEach(options, id: \.self) { option in
-					OptionButton(option: option, item: selectedItem.back!, next: next, isWrong: $isWrong)
-						.padding(.bottom, 15)
-				}
-				Spacer()
-					.frame(height: 45)
+                    .frame(height: 60)
+                ForEach(options, id: \.self) { option in
+                    OptionButton(option: option, item: selectedItem.back!, next: next, isWrong: $isWrong)
+                        .padding(.bottom, 15)
+                }
+                Spacer()
+                    .frame(height: 45)
                 Button(action: {
                     next()
                 }) {
                     Image(systemName: "arrowtriangle.right.circle.fill")
                         .font(.system(size: 60))
                 }.padding()
-				Spacer()
+                Spacer()
                 Stepper("Frequency: \(frequency)", value: $frequency, in: 0 ... 5, step: 1)
                     .frame(width: 320)
                     .padding(.bottom, 30)
@@ -74,13 +73,13 @@ struct QuizView: View {
 
     private func getOptions() -> [String] {
         var options: [String] = [selectedItem.back!]
-		var baseItems = items.filter { $0 != selectedItem }
+        var baseItems = items.filter { $0 != selectedItem }
         for _ in 0 ... 1 {
             let optionIndex = Int.random(in: 0 ..< baseItems.count)
             options.append(baseItems[optionIndex].back!)
-			baseItems = baseItems.filter { $0 != baseItems[optionIndex] }
+            baseItems = baseItems.filter { $0 != baseItems[optionIndex] }
         }
-		return options.shuffled()
+        return options.shuffled()
     }
 
     private func next() {
@@ -115,32 +114,54 @@ struct QuizView: View {
             }
         }
     }
+}
 
-    private struct OptionButton: View {
-        let option: String
-		let item: String
-		let next: () -> Void
-		@Binding var isWrong: Bool
-		
-		private func handleOnClick() {
-			if option != item {
-				isWrong = true
-			} else {
-				next()
-			}
+private struct OptionButton: View {
+    let option: String
+    let item: String
+    let next: () -> Void
+    @Binding var isWrong: Bool
+	@State private var currentColorIsBlue: Bool = true
+
+	private func changeColorAnimation() {
+		currentColorIsBlue = false
+		withAnimation(Animation.linear(duration: 0.2).delay(0.2)) {
+		   currentColorIsBlue = true
 		}
-		
-        var body: some View {
-            Button(action: { handleOnClick() }) {
+	}
+	
+    private func handleOnClick() {
+        if option != item {
+            isWrong = true
+			changeColorAnimation()
+        } else {
+            next()
+        }
+    }
+
+    var body: some View {
+        Button(action: { handleOnClick() }) {
+            ZStack {
                 Text(option)
                     .font(.title)
                     .fontWeight(.bold)
-                    .foregroundColor(.blue)
+					.foregroundColor(.blue)
+                    .frame(width: 160, height: 68)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+							.stroke(.blue, lineWidth: 5)
+                    )
+					.opacity(currentColorIsBlue ? 1 : 0)
+				Text(option)
+					.font(.title)
+					.fontWeight(.bold)
+					.foregroundColor(.red)
 					.frame(width: 160, height: 68)
 					.overlay(
-						   RoundedRectangle(cornerRadius: 40)
-							.stroke(.blue, lineWidth: 5)
-					   )
+						RoundedRectangle(cornerRadius: 40)
+							.stroke(.red, lineWidth: 5)
+					)
+					.opacity(currentColorIsBlue ? 0 : 1)
             }
         }
     }
